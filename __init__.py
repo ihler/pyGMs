bb3ddadf8b1fb29501dd329539fc16c9b6b69f5a
Alexander Ihler <ihler@ics.uci.edu>
2016-04-27 16:50:08 -0700

    missing init file

---
+++ __init__.py
@@ -0,0 +1,18 @@
+"""
+pyGM: Python Graphical Model code
+
+A simple graphical model class for learning about, testing, and developing algorithms
+for graphical models.
+
+Version 0.0.1 (2015-09-28)
+(c) 2015 Alexander Ihler under the FreeBSD license; see license.txt for details.
+"""
+
+import numpy as np;
+from sortedcontainers import SortedSet as sset;
+
+from .factor import *
+from .graphmodel import *
+from .filetypes import *
+
+
