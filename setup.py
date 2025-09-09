505236790867b7d76e3e2af0e2db18aa8d94a10b
Alexander Ihler <atihler@gmail.com>
2025-09-09 14:05:48 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("readme.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='pygms',
+    name='pyGMs',
     version='0.3.4',
     author='Alexander Ihler',
     author_email='ihler@ics.uci.edu',
